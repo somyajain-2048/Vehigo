0938614ce0e6c04b8dc853e6956b35a23d504e12
Namiiii-sys <Namitamehra207@gmail.com>
2025-07-24 04:59:39 +0530

    Blog Navigation arrows & author image

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -34,3 +34,23 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 10 ? header.classList.add("active")
     : header.classList.remove("active");
 });
+
+/**
+ * Triggering navigation buttons scroll
+ */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const blogList = document.querySelector(".blog-list");
+    const leftArrow = document.querySelector(".left-arrow");
+    const rightArrow = document.querySelector(".right-arrow");
+
+    const scrollAmount = 340; 
+
+    rightArrow.addEventListener("click", () => {
+      blogList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+
+    leftArrow.addEventListener("click", () => {
+      blogList.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+  });

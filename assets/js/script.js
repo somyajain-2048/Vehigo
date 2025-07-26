e516d43d776702ce523534e9810d2cb4d3646978
Peehu Mishra <peehumishra1308@gmail.com>
2025-07-26 21:06:01 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,6 +57,19 @@ document.querySelectorAll(".eye-icon").forEach(eyeIcon => {
   });
 });
 
+const toggleBtn = document.getElementById('theme-toggle');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  // Change icon
+  if (document.body.classList.contains('dark-mode')) {
+    toggleBtn.textContent = '☀️'; // Light mode icon
+  } else {
+    toggleBtn.textContent = '🌙'; // Dark mode icon
+  }
+});
+
 
 /**
  * header active on scroll

d27efc012ef3a5ed9da055774bc6445c17e7b3ed
swayam agarwal <you@example.com>
2025-08-17 15:11:33 +0530

    added a getprofile controller and maintained routes

--- vehigo-backend/src/routes/authRoutes.js
+++ vehigo-backend/src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const authMiddleware=require("../middleware/authMiddleware")
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
+
+router.get("/profile", authMiddleware, authController.getProfile);
+router.put("/profile", authMiddleware, authController.updateProfile);
 
 module.exports = router;
